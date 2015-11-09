33e58761fcba09fcef13a72bc5642c1d81ebf735
ErinEMcC <erinm35@gmail.com>
2015-11-09 18:26:47 -0500

    dictionary attempt
    can't parse contents of the div

--- CircusScrape_AndCsv.py
+++ CircusScrape_AndCsv.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-ids = range (0,3,1)
+ids = range (0,1,1)
 for x in ids:
     url = ('http://emuseum.ringling.org/emuseum/view/objects/asitem/16579/'+str(x))
 
@@ -19,69 +19,21 @@ for x in ids:
 
     soup = BeautifulSoup(object_html, "html.parser")
 
-    poster_details = {}
+    posters = []     
 
-    
     #this step just removes (what I think is) an unnecessary extra div
     all_detail = soup.find_all("div", attrs = {"class": "paragraph"})
-    
-    #this is the div inside the div - which is the same info, but cleaner
-    #for details in all_detail:
-        #data = details.find("div")
-        
-    #print ('\n')
 
-    ##Tried using 'nextSibling' to lose the labels (which are inside a span
-    ##inside the div) Only able to pull back the first few data points,
-    ##stopping after "Maker:". Error msg reads: "AttributeError: 'NoneType'
-    ##object has no attribute 'nextSibling'" Something to do with the html
-    ##structure itself and not the code, but an try/except escapes the hitch.
-
-with open("data/src-circus-posters.tsv", "w") as f:
-        fieldnames = ("title", "date", "maker", "dimensions", "medium", "credit", "description", "object")
-
-    output = csv.writer(f, delimiter="\t")
-    output.writerow(fieldnames)
+    a_poster = {}
 
     for details in all_detail:
         label = details.find("span")
         try:
-            print(label.nextSibling)
-        except(AttributeError):
-            print ("BLANK SPAN - SKIP")
+            print (label.text)
+        except AttributeError:
+            continue 
 
-        title = soup.find("label.nextSibling").em.a.encode_contents()
+        if label.text == 'Title':
+            a_poster['Title'] = label.nextSibling
 
-    
-       
-        output.writerow(fieldnames)
-
-
-
-
-
-
-
-        #poster=[title, date, link, BLANK, description, id]
-        #posters_data.append(poster)
-
-
-#import csv
-
-#with open ('filename.cv', 'wb') as file:
-#           writer=csv.writer(file)
-           #for row in posters_data:
-            #   writer.writerow(row)
-           
-
-        
-
-#time.sleep(10) - this should maybe go in somewhere(?) to pause between pulls.
-
-
-
-
-
-        
-
-     
+    print (a_poster)
