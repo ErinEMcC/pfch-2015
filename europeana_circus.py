629f9e85420e077d397be888def9f96f30eda420
ErinEMcC <erinm35@gmail.com>
2015-11-13 09:27:48 -0500

    Europeana API Vintage Circus Poster collection from NL
    Basic query to JSON

--- europeana_circus.py
+++ europeana_circus.py
@@ -1,7 +1,7 @@
 import requests
 import json
  
-r = requests.get('http://www.europeana.eu/api/v2/search.json?wskey=JQ4CNwmGL&query=europeana_collectionName%3A2021603*&qf=YEAR%3A%5B1950+TO+1955%5D&start=1&rows=1&profile=standard')
+r = requests.get('http://www.europeana.eu/api/v2/search.json?wskey=JQ4CNwmGL&query=europeana_collectionName%3A2021603*&qf=YEAR%3A%5B1900+TO+1901%5D&start=1&rows=12&profile=standard')
 
 print(r.status_code)
 print ("\n")
@@ -10,5 +10,7 @@ print(r.text)
 print ("\n")
 
 #turn it into a python dictonary
-data = json.loads(r.text)
-print(data.keys())
+#data = json.loads(r.text)
+#print(data.keys())
+
+
