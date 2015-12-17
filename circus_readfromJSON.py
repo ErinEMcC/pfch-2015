f3aa69f499a10bf71f0b162db90f6d11f07f28cb
ErinEMcC <erinm35@gmail.com>
2015-12-16 21:21:43 -0500

    Reading, Collecting content from JSON
    includes finding, printing just the title key value from list of
    dictionaries
    then a regex for reading everything before the colon from the
    titles-only list

--- circus_readfromJSON.py
+++ circus_readfromJSON.py
@@ -1,6 +1,30 @@
 import json
+import pprint
+import re
 
-with open('scraped_circusposters.json', 'r') as f:
-     data = json.load(f)
+##opening json file to read, parse 
+with open('scraped_circusposters.json') as json_data:
+     data = json.load(json_data)
+     json_data.close()
+     
+     ##PRETTY PRINTING TO CHECK SITUATION
+     ##pprint.pprint(data)
 
-     print (data[""][""]
+    ## gets all key values in list of dictionaries when key = title
+     for i in (data):
+         titles = (i['title'])
+
+         ## regex for content before a colon ^[^:]+\s*
+         ## 90% of the archive this is troupe name
+         troupes = re.findall('^[^:]+\s*', titles)
+         print (troupes)
+
+         ## writes results back into separate json file        
+         with open ('troupes.json', 'w') as f:
+             f.write(json.dumps(troupes, indent=4))
+
+         
+         
+         
+         
+         
